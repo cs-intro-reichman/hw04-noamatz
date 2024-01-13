44dd54e76943e66068e0454b21d4b3bf197d7ad7
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2024-01-13 16:35:08 +0200

    Check for first assignent

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,76 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+		System.out.println(isSorted(new int[] {1,3,2}));
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
-    }
-
-    public static int secondMaxValue(int [] array) {
-        // Write your code here:
+		int[] ideal = new int[(array.length + 1)];
+		for (int i = 0; i < array.length + 1; i++) {
+			ideal[i] = i;
+		}
+		for (int i = 0; i < array.length; i++) {
+			if (ideal[i] != array[i]) {
+				return i;
+			}
+		}
         return 0;
     }
 
+    public static int secondMaxValue(int [] array) {
+        int max = 0;
+		int secondMax = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] >= max && array[i] > secondMax) {
+				max = array[i];
+			} 
+		}
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > secondMax && array[i] != max) {
+				secondMax = array[i];
+			} 
+		}
+		return (secondMax);
+    }
+
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+		boolean equal = true;
+		for (int i = 0; i < array1.length; i++) {
+			equal = false;
+			for (int j = 0; j < array2.length; j++) {
+				if (array1[i] == array2[j]) {
+					equal = true;
+				}
+			}
+			if (equal == false) {
+				break;
+			}
+		}
+		return equal;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+	int max = array[0];
+	boolean checkInc = true;
+	boolean checkDec = true;
+	//Check if its increasing
+	for (int i = 0; i < array.length; i++) {
+		if (max > array[i]) {
+			checkInc = false;
+		}
+		max = array[i];
+	}
+	//Check if its decreasing
+	max = array[0];
+	for (int i = 0; i < array.length; i++) {
+		if (max < array[i]) {
+			checkDec = false;
+		}
+		max = array[i];
+	}
+        if (checkInc == false && checkDec == false) {
+			return false;
+		} else { 
+			return true;
+		}
     }
-
 }

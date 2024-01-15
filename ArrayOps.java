090ebdd2917ef034fdf9398fc1fee6bb89ed066e
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2024-01-15 17:42:07 +0200

    Done

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,6 +1,6 @@
 public class ArrayOps {
     public static void main(String[] args) {
-		System.out.println(isSorted(new int[] {1,3,2}));
+		System.out.println(secondMaxValue(new int[] {2, 8, 3, 7, 8}));
     }
     
     public static int findMissingInt (int [] array) {
@@ -8,8 +8,15 @@ public class ArrayOps {
 		for (int i = 0; i < array.length + 1; i++) {
 			ideal[i] = i;
 		}
-		for (int i = 0; i < array.length; i++) {
-			if (ideal[i] != array[i]) {
+		boolean found1 = false;
+		for (int i = 0; i < ideal.length; i++) {
+			found1 = false;
+			for (int j = 0; j < array.length; j++) {
+				if (ideal[i] == array[j]) {
+					found1 = true;
+				}
+			}
+			if (found1 == false) {
 				return i;
 			}
 		}
@@ -19,16 +26,19 @@ public class ArrayOps {
     public static int secondMaxValue(int [] array) {
         int max = 0;
 		int secondMax = 0;
+		int indexMax = 0;
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] >= max && array[i] > secondMax) {
 				max = array[i];
+				indexMax = i;
 			} 
 		}
 		for (int i = 0; i < array.length; i++) {
-			if (array[i] > secondMax && array[i] != max) {
+			if (array[i] > secondMax && i != indexMax) {
 				secondMax = array[i];
 			} 
 		}
+		
 		return (secondMax);
     }
 
@@ -48,7 +58,7 @@ public class ArrayOps {
 		return equal;
     }
 
-    public static boolean isSorted(int [] array) {
+public static boolean isSorted(int [] array) {
 	int max = array[0];
 	boolean checkInc = true;
 	boolean checkDec = true;
